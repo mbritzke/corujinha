55537cfdd3fb96847af8519e22ed89aa7964a75f
Matheus Cosme Britzke <ilegra.britzke@agibank.com.br>
2020-09-20 22:32:08 -0300

    Endpoint getAllWords melhorado

--- corujinha-back-end/src/main/java/com/github/mbritzke/controller/KeyWordController.java
+++ corujinha-back-end/src/main/java/com/github/mbritzke/controller/KeyWordController.java
@@ -1,19 +1,17 @@
 package com.github.mbritzke.controller;
 
 import com.github.mbritzke.dto.KeyWordDto;
-import com.github.mbritzke.entity.KeyWordEntity;
 import com.github.mbritzke.exception.EmptyKeyWordException;
-import com.github.mbritzke.repository.KeyWordRepository;
 import com.github.mbritzke.service.KeyWordService;
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.ResponseBody;
 import org.springframework.web.bind.annotation.RestController;
+
+import java.util.List;
 
 @RestController
 @RequestMapping("/keywords")
@@ -25,9 +23,6 @@ public class KeyWordController {
         this.keyWordService = keyWordService;
     }
 
-    @Autowired
-    private KeyWordRepository keyWordRepository;
-
     @PostMapping(path = "/add")
     public ResponseEntity<KeyWordDto> addNewWord(@RequestBody KeyWordDto keyWordDto) throws EmptyKeyWordException {
         try {
@@ -38,8 +33,7 @@ public class KeyWordController {
     }
 
     @GetMapping(path = "/all")
-    public @ResponseBody
-    Iterable<KeyWordEntity> getAllWords() {
-        return keyWordRepository.findAll();
+    public ResponseEntity<List<KeyWordDto>> getAllWords() {
+        return ResponseEntity.ok(keyWordService.getAllKeyWords());
     }
 }
