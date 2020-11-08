f185af1d4057320270e8d0cb1f06db88431ac86d
Matheus Cosme Britzke <ilegra.britzke@agibank.com.br>
2020-11-08 12:02:52 -0300

    Mensagem atualizada

--- corujinha-back-end/src/main/java/com/github/mbritzke/exception/EmptyKeyWordException.java
+++ corujinha-back-end/src/main/java/com/github/mbritzke/exception/EmptyKeyWordException.java
@@ -3,6 +3,6 @@ package com.github.mbritzke.exception;
 public class EmptyKeyWordException extends RuntimeException{
 
     public EmptyKeyWordException() {
-        super("keyword not found");
+        super("Keyword not found");
     }
 }
