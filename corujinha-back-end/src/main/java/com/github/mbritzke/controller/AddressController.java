f0e69d9c61b92ee1ba1d80a4a265d223bafec94f
Matheus Cosme Britzke <ilegra.britzke@agibank.com.br>
2020-11-08 11:57:41 -0300

    Nome do endpoint corrigido

--- corujinha-back-end/src/main/java/com/github/mbritzke/controller/AddressController.java
+++ corujinha-back-end/src/main/java/com/github/mbritzke/controller/AddressController.java
@@ -13,7 +13,7 @@ import org.springframework.web.bind.annotation.RestController;
 import java.util.List;
 
 @RestController
-@RequestMapping("/address")
+@RequestMapping("/addresses")
 public class AddressController {
 
     private final AddressService addressService;
